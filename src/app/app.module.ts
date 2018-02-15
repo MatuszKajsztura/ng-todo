5b5d9c61392a110014c9ce5b7540d63c0d8cc757
Mateusz Kajsztura <mkajsztura@davinci-studio.eu>
2018-02-15 13:39:12 +0100

    Code review fix: reorder imports (app-routing.module and app.module),add redirection to home in every case (app-routing.module),reformat and set ascending order in <a> (home.component.html), add own class, reformat (list.component.scss),add spacing between import and styles (styles.scss), add new line at file’s end if there wasn’t.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material';
 
-
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
-import { MatButtonModule } from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent,

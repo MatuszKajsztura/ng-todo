f0ae0918e0695349d3bd4be745b2594deab2856e
Mateusz Kajsztura <mkajsztura@davinci-studio.eu>
2018-02-15 18:05:37 +0100

    Code review fix, remove slash,spaces and add new line

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
 
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';

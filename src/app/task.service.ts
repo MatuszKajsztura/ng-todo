0d9e7c11724b7af273a2dac364c5632b65ea71d5
Mateusz Kajsztura <mkajsztura@davinci-studio.eu>
2018-09-28 11:43:31 +0200

    add local storage to store tasks, refactoring

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -4,58 +4,55 @@ import { map } from 'rxjs/operators';
 
 import { Task } from './task';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class TaskService {
+  private nextId = 1;
 
-  private TASKS: Task[] = [
-      new Task('1', 'zadanie 1', new Date(2018, 1, 2)),
-      new Task('2', 'zadanie 2', new Date(2018, 10, 20), true),
-    ];
+  private todos: Task[] = [];
 
-  private tasksSubject = new BehaviorSubject<Task[]>(this.TASKS);
+  private tasksSubject = new BehaviorSubject<Task[]>(this.todos);
 
   constructor() { }
 
   getTasks(): Observable<Task[]> {
-    // return of(JSON.parse(JSON.stringify(this.TASKS)));
-    // this.tasksSubject.next(this.TASKS);
+
+    const localStorageItem = JSON.parse(localStorage.getItem('todos'));
+
+    if (localStorageItem.length) {
+      this.tasksSubject.next(localStorageItem);
+      this.todos = localStorageItem;
+      this.nextId = this.todos[this.todos.length - 1].id + 1;
+    }
+
     return this.tasksSubject.asObservable();
   }
 
   updateTask(e): void {
-    const whichTask = this.TASKS.find(t => t.id === e.id); // przypisuje do zmiennej whichTask obiekt ktory spełnia warunek
-    // console.log('Serwis aktualizuje zadanie nr: ' + whichTask.id);
+    const whichTask = this.todos.find(t => t.id === e.id); // przypisuje do zmiennej whichTask obiekt ktory spełnia warunek
     whichTask.name = e.name;
     whichTask.done = e.done;
     whichTask.date = e.date;
-    console.log('UPDATE, Aktualny stan w serwisie: ', this.TASKS);
-    this.tasksSubject.next(this.TASKS);
+
+    this.updateState();
     }
 
-  // updateTask(e): Observable<Task> {
-  //   // const whichTask = this.TASKS.find(t => t.id === e.id);
-  //   return this.TASKS
-  //     .map((t) => {
-  //         t.date = e.date
-  //         t.done = e.done
-  //         t.name = e.name
-  //       });
-  //   }
-
   addTask(task: Task): void {
-    const id = 'id-' + Math.random().toString(36).substr(2, 16);
-    this.TASKS.push( new Task(id, task.name, task.date, task.done));
-    console.log('ADD, Aktualny stan w serwisie: ', this.TASKS);
-    this.tasksSubject.next(this.TASKS);
+    this.todos.push(new Task(this.nextId, task.name, new Date(), task.done));
+    this.nextId++;
+
+    this.updateState();
   }
 
   deleteTask(task: Task): void {
-    // console.log('Serwis usuwa zadanie: ' + task.id);
-    this.TASKS = this.TASKS.filter(e => e.id !== task.id);
-    console.log('DELETE, Aktualny stan w serwisie: ', this.TASKS);
-    this.tasksSubject.next(this.TASKS);
+    this.todos = this.todos.filter(e => e.id !== task.id);
+
+    this.updateState();
+    }
+
+    updateState(): void {
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+      this.tasksSubject.next(this.todos);
     }
 }
